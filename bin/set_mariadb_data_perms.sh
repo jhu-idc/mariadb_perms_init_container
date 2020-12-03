7140d3eeef01872b125f593c002910c27bea29c4
Derek Belrose <derek@derekbelrose.com>
2020-12-03 13:52:14 -0500

    Adding workflow and shell script

---
+++ bin/set_mariadb_data_perms.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIRECTORY=$MARIADB_INIT_DIRECTORY
+UID=$MARIADB_UID
+GID=$MARIADB_GID
+
+COMBOUGID="$2:$3"
+
+res=`stat -c "%u:%g" $DIRECTORY`
+
+if [ $res != $COMBOUGID ]; then
+    chown $COMBOUGID -R $DIRECTORY
+fi
